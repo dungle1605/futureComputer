1dbd25d5e680cc89433203f7ba6106e22ea8bb77
jaywong012 <125846034+jaywong012@users.noreply.github.com>
2023-02-21 23:47:08 +0700

    Create GetAllCategories API & UnitTest

---
+++ src/FutureComputer.Application/Categories/GetAllCategories/GetAllCategoriesQuery.cs
@@ -0,0 +1,9 @@
+﻿using FutureComputer.Application.Categories.Common;
+using MediatR;
+
+namespace FutureComputer.Application.Categories.GetAllCategories
+{
+    public class GetAllCategoriesQuery : IRequest<List<CategoryResponse>>
+    {
+    }
+}
