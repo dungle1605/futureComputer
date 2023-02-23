cd524aafd5e450feb9cd96b9a9e41c61990b5b88
jaywong012 <125846034+jaywong012@users.noreply.github.com>
2023-02-23 22:48:35 +0700

    Add validator for update delete category

---
+++ src/FutureComputer.Application/Categories/UpdateCategory/UpdateCategoryValidation.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using FutureComputer.Application.Categories.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureComputer.Application.Categories.UpdateCategory
+{
+    public class UpdateCategoryValidation : AbstractValidator<UpdateCategoryCommand>
+    {
+        [Obsolete]
+        public UpdateCategoryValidation()
+        {
+            RuleFor(x => x.Id)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage(string.Format(CommonConstant.ERROR_REQUEST_MESSAGE, "UpdateCategory"));
+
+            RuleFor(x => x.Name)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage(string.Format(CommonConstant.ERROR_REQUEST_MESSAGE, "UpdateCategory"));
+        }
+    }
+}
